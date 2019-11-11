6434af125bf82058266095ded7d56bde37e2fa9a
Gitte Vermeiren <Gitte.Vermeiren@microsoft.com>
2019-11-11 21:57:33 +0000

    fixed errors

--- src/weather-service/src/main/resources/import.sql
+++ src/weather-service/src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO `azurespringcloudtraining`.`weather` (`city`, `description`, `icon`) VALUES ('Brussels, BE', 'Very cloudy!', 'weather-fog');
-INSERT INTO `azurespringcloudtraining`.`weather` (`city`, `description`, `icon`) VALUES ('Paris, France', 'Very cloudy!', 'weather-fog');
-INSERT INTO `azurespringcloudtraining`.`weather` (`city`, `description`, `icon`) VALUES ('London, UK', 'Quite cloudy', 'weather-pouring');
+INSERT INTO `azurespringcloudtraining`.`weather` (`city`, `description`, `icon`) VALUES ('Brussels BE', 'Very cloudy!', 'weather-fog');
+INSERT INTO `azurespringcloudtraining`.`weather` (`city`, `description`, `icon`) VALUES ('Paris France', 'Very cloudy!', 'weather-fog');
+INSERT INTO `azurespringcloudtraining`.`weather` (`city`, `description`, `icon`) VALUES ('London UK', 'Quite cloudy', 'weather-pouring');
