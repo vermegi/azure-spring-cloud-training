ea1bcdbcb9632ea8e29886f4033f21868b77c5d1
Gitte Vermeiren <Gitte.Vermeiren@microsoft.com>
2019-11-11 09:42:00 +0000

    finished demo 5

--- src/spring-cloud-microservice/src/main/java/com/example/demo/DemoApplicationSpringConfig.java
+++ src/spring-cloud-microservice/src/main/java/com/example/demo/DemoApplicationSpringConfig.java
@@ -3,14 +3,10 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplicationSpringConfig {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DemoApplicationSpringConfig.class, args);
 	}
-
 }
-
-
