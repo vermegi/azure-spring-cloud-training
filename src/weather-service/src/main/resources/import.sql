c00affbb003fd71568fbb8813f3841659677828d
Gitte Vermeiren <Gitte.Vermeiren@microsoft.com>
2019-11-12 06:09:46 +0000

    demo 10

--- src/weather-service/src/main/resources/import.sql
+++ src/weather-service/src/main/resources/import.sql
@@ -1,3 +1,4 @@
 INSERT INTO `azurespringcloudtraining`.`weather` (`city`, `description`, `icon`) VALUES ('Brussels BE', 'Very cloudy!', 'weather-fog');
 INSERT INTO `azurespringcloudtraining`.`weather` (`city`, `description`, `icon`) VALUES ('Paris France', 'Very cloudy!', 'weather-fog');
 INSERT INTO `azurespringcloudtraining`.`weather` (`city`, `description`, `icon`) VALUES ('London UK', 'Quite cloudy', 'weather-pouring');
+INSERT INTO `azurespringcloudtraining`.`weather` (`city`, `description`, `icon`) VALUES ('Amsterdam NL', 'Quite cloudy', 'weather-pouring');
